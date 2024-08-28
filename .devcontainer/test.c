0714f6101559073f48377e55384f92b57616c699
abwerth <awerth1@live.spcollege.edu>
2024-08-28 02:17:00 +0000

    first commit

---
+++ .devcontainer/test.c
@@ -0,0 +1,24 @@
+/*
+* ENUM Example Program
+*/
+
+#include<stdio.h>
+enum year{Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+int main()
+{
+    enum year i ;
+    i = Jan;
+    printf("%d\n",i);
+    i = Feb;
+    printf("%d\n",i);
+    i = Mar;
+    printf("%d\n",i);
+    i = Apr;
+    printf("%d\n",i);
+    i = May;
+    printf("%d\n",i);
+    i = Jun;
+    printf("%d\n",i);
+
+}
